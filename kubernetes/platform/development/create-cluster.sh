708a107e3ca61c57131dba4b851616ae8fb1e096
anchevski <andrej.anchevski@rtl-extern.de>
2023-01-24 01:15:21 +0100

    NOTICKET - Added redis deployment, scripts for easy setting up a cluster

---
+++ kubernetes/platform/development/create-cluster.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+echo "\n Initializing Kubernetes cluster...\n"
+
+minikube start --cpus 2 --memory 4g --driver docker --profile polar
+
+echo "\n Enabling NGINX Ingress Controller...\n"
+
+minikube addons enable ingress --profile polar
+
+sleep 15
+
+echo "\n Deploying platform services..."
+
+kubectl apply -f services
+
+sleep 5
+
+echo "\n Waiting for PostgreSQL to be deployed..."
+
+while [ $(kubectl get pod -l db=polar-postgres | wc -l) -eq 0 ] ; do
+  sleep 5
+done
+
+echo "\n Waiting for PostgreSQL to be ready..."
+
+kubectl wait \
+  --for=condition=ready pod \
+  --selector=db=polar-postgres \
+  --timeout=180s
+
+echo "\n Waiting for Redis to be deployed..."
+
+while [ $(kubectl get pod -l db=polar-redis | wc -l) -eq 0 ] ; do
+  sleep 5
+done
+
+echo "\n⌛ Waiting for Redis to be ready..."
+
+kubectl wait \
+  --for=condition=ready pod \
+  --selector=db=polar-redis \
+  --timeout=180s
+
+echo "\n Happy Sailing!\n"
---
+++ kubernetes/platform/development/destroy-cluster.sh
@@ -0,0 +1,7 @@
+echo "\n️ Destroying Kubernetes cluster...\n"
+
+minikube stop --profile polar
+
+minikube delete --profile polar
+
+echo "\n️ Cluster destroyed\n"
